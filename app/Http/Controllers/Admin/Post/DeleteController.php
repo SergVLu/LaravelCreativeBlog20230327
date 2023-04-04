399acebeed8087bb1a0e7cee0b47d37e7297072d
SergVLu <rumasyba@gmail.com>
2023-04-04 13:22:18 +0300

    delete unusual links at DeletCont-r lesson20

--- app/Http/Controllers/Admin/Post/DeleteController.php
+++ app/Http/Controllers/Admin/Post/DeleteController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers\Admin\Post;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 use App\Models\Post;
-use App\Http\Requests\Admin\Post\UpdateRequest;
+// use App\Http\Requests\Admin\Post\UpdateRequest;
 
 class DeleteController extends Controller
 {
